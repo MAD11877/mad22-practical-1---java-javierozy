6e53aa64d2876430faf91e9a79cb9cd0da45cb05
javierozy <77658238+javierozy@users.noreply.github.com>
2022-04-21 00:04:27 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,17 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter an integer:");
+    int num = in.nextInt();
+    int i,j;
+    for(i=num;i>=1;i--)
+    		{
+        		for(j=1;j<=i;j++)
+        		{
+            			System.out.print("* ");
+        		}
+        		System.out.println("");
+    		}	
     
   }
 }
