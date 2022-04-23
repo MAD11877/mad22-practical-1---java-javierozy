9708c014525147f07f417af4d3c46ac6e9c060ab
javierozy <77658238+javierozy@users.noreply.github.com>
2022-04-23 15:45:15 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,7 +4,7 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
+    /*
      * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
      * Print out the mode (highest occurrence) from the set of integers. 
      *    e.g.
@@ -27,6 +27,54 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    System.out.println("Please enter an integer:");
+    int num = in.nextInt();
+    int[] b = new int[num];
+    int i = 0;
+
+    System.out.println("Please enter a set of integer:");
+
+    /*while loop is used to cpature all the inputs*/
+    while (i < num){
+
+      int tempnum = in.nextInt();
+
+      b[i] = tempnum;
+
+      i += 1;
+    }
+
+    int maxint = 0;
+    int maxoccurrence = 0;
+    
+    /*looking for integer with the max. occurrence*/
+    for (int k = 0; k < b.length; k++){
+
+      int tempnum = b[k];
+      int tempoccurrence = 0;
+
+      /*doing comparesion*/
+      for (int l = 0; l < b.length; l++){
+        
+        if (tempnum == b[l]){
+  
+          tempoccurrence += 1;
+          
+        }
+      }
+
+       /*doing overwritting*/
+      if (maxoccurrence < tempoccurrence){
+
+        maxoccurrence = tempoccurrence;
+        maxint = b[k];
+        
+      }
+    }
+
+    /*displaying*/
+    System.out.println("Integer: " + Integer.toString(maxint));
     
   }
 }
