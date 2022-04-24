5656bd45abd58fd5550d6aced641af9b48c6bfaa
javierozy <77658238+javierozy@users.noreply.github.com>
2022-04-25 00:08:32 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -17,7 +17,7 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
+    /**Scanner in = new Scanner(System.in);
     int num = in.nextInt();
     int i,j;
     for(i=num;i>=1;i--)
@@ -26,7 +26,36 @@ public class Question4
         		{
             			System.out.print("*");
         		}
-        		System.out.println("");
+        		System.out.println("");**/
+     Scanner in = new Scanner(System.in);
+
+  
+    int num = in.nextInt();
+
+    int i = 1;
+    String tri = ("");
+
+
+    ArrayList<String> list = new ArrayList<>();
+
+
+    while(i <= num){
+
+      tri += "*";
+      list.add(tri);
+
+      i++;
+
+    }
+
+
+    for (int k = list.size()-1; k > -1; k--){
+
+        System.out.println(list.get(k));
+
+      }
+  }
+}
     		}	
     
   }
